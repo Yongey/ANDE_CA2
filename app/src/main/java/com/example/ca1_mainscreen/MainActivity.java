232e216d1ebfcbc43fceacf08f9dbad696e5333f
Yongey <yangzeyi4@gmail.com>
2024-01-21 23:22:46 +0800

    ca2 files

--- app/src/main/java/com/example/ca1_mainscreen/MainActivity.java
+++ app/src/main/java/com/example/ca1_mainscreen/MainActivity.java
@@ -1,36 +1,19 @@
 package com.example.ca1_mainscreen;
-
-import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import com.example.ca1_mainscreen.databinding.ActivityMainBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-
 public class MainActivity extends AppCompatActivity {
 
 FirebaseAuth auth;
